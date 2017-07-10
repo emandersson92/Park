6926eb5a78d871c85d3a1da1b07eeb22dd99b392
Emil <emandersson92@gmail.com>
2017-07-10 13:01:38 +0200

    first

---
+++ Park/Background.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+
+
+class Background
+{
+public:
+	Background();
+	~Background();
+
+	int calcHist(char* img);
+};
+
