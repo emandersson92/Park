b4fc88888da09c7fe82db964d5890cfed3bea63b
Emil <emandersson92@gmail.com>
2017-07-18 14:38:51 +0200

    Implemented Filter class

--- Park/Filter.cpp
+++ Park/Filter.cpp
@@ -2,27 +2,50 @@
 #include "Filter.h"
 
 
-Filter::Filter()
-{
+Filter::Filter(){
+	init();
 }
 
 
-Filter::~Filter()
-{
+Filter::~Filter(){
 }
 
-void Filter::apply() {
+
+
+void Filter::apply(Mat& in, Mat& out) {
 	//implement so constructor defines which function to call
+	openFilter(in, out);
 
 }
 
-void Filter::openFilter(Mat& img) {
-	Mat tmp;
-	erode(img, tmp);
-	dilate();
+void Filter::openFilter(Mat& in, Mat& out) {
+	erode(in, out, erElem);
+	dilate(in, out, diElem);
 
 }
-void Filter::closeFilter(Mat& img) {
-	dilate();
-	erode();
+void Filter::closeFilter(Mat& in, Mat& out) {
+	dilate(in, out, diElem);
+	erode(in, out, erElem);
+}
+
+
+
+void Filter::init() {
+	int erShape = MORPH_RECT;
+	int diShape = MORPH_RECT;
+
+	int erosion_elem = 0;
+	int erosion_size = 5;			//shall be adaptive in the future
+	int dilation_elem = 0;
+	int dilation_size = 5;			//shall be adaptive in the future
+
+
+	erElem = getStructuringElement(erShape,
+		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		Point(erosion_size, erosion_size));
+
+	diElem = getStructuringElement(diShape,
+		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+		Point(dilation_size, dilation_size));
+
 }
--- Park/Filter.h
+++ Park/Filter.h
@@ -1,8 +1,25 @@
 #pragma once
+#include "opencvIncludes.h"
+
+
 class Filter
 {
 public:
-	Filter();
+	Filter();								//define function to run when apply() is called 
 	~Filter();
+
+	void apply(Mat& in, Mat& out);			//run function defined in constructor
+
+
+private:
+	void init();
+
+	void openFilter(Mat& in, Mat& out);
+	void closeFilter(Mat& in, Mat& out);
+
+
+	Mat erElem;								//Erode element
+	Mat diElem;								//Dilate element
+
 };
 
