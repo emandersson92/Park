7d7043f93f6990c6cf22d6641c69ed48438b5166
Emil <emandersson92@gmail.com>
2017-07-20 23:49:20 +0200

    Added VehicleDetector class

---
+++ Park/VehicleDetector.h
@@ -0,0 +1,30 @@
+/********************************
+Info:
+This class is a template of a vehile detector
+The class can be used in tracker objects
+
+Author:
+Emil Andersson 2017-07-DATE
+********************************/
+#pragma once
+#include "opencvIncludes.h"
+
+class VehicleDetector
+{
+public:
+	VehicleDetector();
+	~VehicleDetector();
+	
+	//The parts that must be included in a VehicleDetector-abstraction 
+
+
+private:
+
+	virtual void imgAquist(Mat& in, Mat& out) = 0;
+	virtual void segment(Mat& in, Mat& out) = 0;
+	virtual void filter(Mat& in, Mat& out) = 0;
+	virtual void classify(Mat& in, Mat& out) = 0;
+	
+
+};
+
