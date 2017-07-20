092da28bc5d4d9326b8d66f8ee7635bb62a5ea34
Emil <emandersson92@gmail.com>
2017-07-20 23:47:56 +0200

    Added a Vehicle class which contains VehicleFrames

---
+++ Park/Vehicle.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "opencvIncludes.h"
+#include "VehicleFrame.h"
+#include "VehicleList.h"
+
+class Vehicle
+{
+public:
+	Vehicle();
+	~Vehicle();
+
+	//Functions used by lists to determine if they belong
+	void getSpeed();
+	void getPosition();
+	//...
+	//...
+	//...
+
+	VehicleList list;
+
+private:
+	vector<VehicleFrame> vehFrames;
+
+};
+
