ac940e2529bdea098e0de74bf78ece9ab7c1f36e
Emil <emandersson92@gmail.com>
2017-07-20 23:48:47 +0200

    Created VehicleFrame

---
+++ Park/VehicleFrame.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "VehicleFrame.h"
+
+
+VehicleFrame::VehicleFrame()
+{
+}
+
+
+VehicleFrame::~VehicleFrame()
+{
+}
---
+++ Park/VehicleFrame.h
@@ -0,0 +1,8 @@
+#pragma once
+class VehicleFrame
+{
+public:
+	VehicleFrame();
+	~VehicleFrame();
+};
+
