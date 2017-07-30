ae91fd249617d7ddda869508bd2221369990586f
Emil <emandersson92@gmail.com>
2017-07-30 09:49:25 +0200

    trying to iterate through vector. not working

--- Park/BinClassify.cpp
+++ Park/BinClassify.cpp
@@ -12,7 +12,7 @@ BinClassify::~BinClassify()
 void BinClassify::apply(cv::Mat& img, std::vector<std::vector<cv::Point>>& contours){
 	//implement so constructor defines which function to call
 	findCont(img, contours);
-	checkArea(contours);
+	//checkArea(contours);
 
 }			
 
@@ -25,18 +25,22 @@ void BinClassify::findCont(cv::Mat& img, std::vector<std::vector<cv::Point>>& co
 
 void BinClassify::checkArea(std::vector<std::vector<cv::Point>>& contours) {
 	
-	std::vector<std::vector<cv::Point>>::const_iterator i;
+	//dont work yet
+
+	std::vector<std::vector<cv::Point>>::iterator i;
 
 	//TODO Using vectors is reducing the speed of the program????
-	for (i = contours.begin(); i!= contours.end(); ++i)
+	for (i = contours.begin(); i != contours.end(); ++i)
 	{
 		cv::Rect2d r = cv::boundingRect(*i);
 
 		///if not right area
-		if(r.width > widMax || r.width < widMin || r.height > heiMax || r.height < heiMin)
+		if (r.width > widMax || r.width < widMin || r.height > heiMax || r.height < heiMin)
 		{
 			//I think this code will increment all elements by one after the removed object. 
 			contours.erase(i);
 		}
 
+
+	}
 }
