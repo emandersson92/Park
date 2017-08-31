cb91ddfc0f92025c4b9bcaee767d7764dca85b9f
Emil <emandersson92@gmail.com>
2017-08-31 17:40:30 +0200

    added class to construct test objects during testing and validation in main

---
+++ Park/Example.cpp
@@ -0,0 +1,24 @@
+#include "Example.h"
+
+
+Example::Example(){
+
+}
+
+Example::Example~(){
+
+}
+
+Vehicle* Example::createVehicle(){
+
+	Timer* timer = new SimpleTimer();
+
+	vector<point> contours = circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+
+	Vehicle* v = e.createVehicle(0.0, 30, 40, timer, contours);
+
+ return v;
+  
+}
+
+
---
+++ Park/Example.h
@@ -0,0 +1,16 @@
+/*
+Used for instance to tests to create example objects
+ */
+
+#include "Vehicle.h"
+
+class Example(){
+
+ public:
+  Vehicle* createVehicle();
+
+
+ private:
+
+}
+
